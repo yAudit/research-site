34705e8489cd045bfb12a5995680de1670d62c43
nullity00 <00nullity@gmail.com>
2025-12-14 05:35:04 +0530

    yarn->pnpm, tooltip font, favicon change, electi->yaudit, footer fixes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -75,22 +75,6 @@ export const Icons = {
       ></path>
     </svg>
   ),
-  telegram: (props: IconProps) => (
-    <svg
-      viewBox="0 0 24 24"
-      width="50px"
-      height="50px"
-      xmlns="http://www.w3.org/2000/svg"
-      {...props}
-    >
-      <path
-        fill="currentColor"
-        d="M19.2,4.4L2.9,10.7c-1.1,0.4-1.1,1.1-0.2,1.3l4.1,1.3l1.6,4.8c0.2,0.5,0.1,0.7,0.6,0.7c0.4,0,0.6-0.2,0.8-0.4
-	c0.1-0.1,1-1,2-2l4.2,3.1c0.8,0.4,1.3,0.2,1.5-0.7l2.8-13.1C20.6,4.6,19.9,4,19.2,4.4z M17.1,7.4l-7.8,7.1L9,17.8L7.4,13l9.2-5.8
-	C17,6.9,17.4,7.1,17.1,7.4z"
-      ></path>
-    </svg>
-  ),
 };
 
 const DATA = {
@@ -109,14 +93,9 @@ const DATA = {
         url: "https://www.github.com/yaudit",
         icon: Icons.github,
       },
-      Telegram: {
-        name: "Telegram",
-        url: "https://t.me/yaudit",
-        icon: Icons.telegram,
-      },
       X: {
         name: "X",
-        url: "https://www.x.com/yaudit",
+        url: "https://www.x.com/yAuditDAO",
         icon: Icons.x,
       },
       youtube: {
